d023178ab344b7748fb411ebec53ded596b890c7
stevenminken <stevenminken@gmail.com>
2023-03-27 22:48:04 +0200

    testklasse Person uitgewerkt

---
+++ src/test/java/PetTest.java
@@ -0,0 +1,38 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PetTest {
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void getAge() {
+    }
+
+    @Test
+    void setAge() {
+    }
+
+    @Test
+    void getSpecies() {
+    }
+
+    @Test
+    void setSpecies() {
+    }
+
+    @Test
+    void getOwner() {
+    }
+
+    @Test
+    void setOwner() {
+    }
+}
